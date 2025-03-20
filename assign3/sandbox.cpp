98787586be3eb25aa333b49d7cd6323d20267093
ErickOlivo <OliauErick@outlook.com>
2025-03-19 22:07:19 -0500

    Implementé sandbox() para instanciar y probar la clase Coche

--- assign3/sandbox.cpp
+++ assign3/sandbox.cpp
@@ -2,7 +2,14 @@
  * CS106L Assignment 3: Make a Class
  * Created by Fabio Ibanez with modifications by Jacob Roberts-Baca.
  */
-#include "Coche.h"
+#include "class.h"
+#include <iostream>
+
 void sandbox() {
+  // Instancia usando el constructor personalizado
   Coche miCoche {"Toyota", "Corolla", 20};
+
+  std::cout << "Marca: " << miCoche.getMarca() << std::endl;
+  std::cout << "Modelo: " << miCoche.getModelo() << std::endl;
+  std::cout << "Velocidad: " << miCoche.getVelocidad() << std::endl;
 }
